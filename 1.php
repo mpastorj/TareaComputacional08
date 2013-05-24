674b5a2474e8ff99f7fa4b68d6cc48093575b019
mpastorj <mrlyn.p.j@gmail.com>
2013-05-24 00:44:38 -0400

    Formulario basico

---
+++ 1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang = "es">
+	<head>
+	    <meta charset = "UTF-8">
+		<title> Tarea 08 ISW  <?php echo date("d/m/Y"); ?> </title>
+		<link rel="stylesheet" href="estilo.css" type="text/css" media="all">
+	</head>
+	
+	<body>
+	<h2>Ingrese:</h2>
+		<form method = "post" action = "2.php">
+			<h3> Usuario: </h3>
+			<input type = "text" name = "usuario"/>
+			<h3> Contraseña: </h3>
+			<input type = "password" name = "contraseña"/>
+			<p> <input type = "submit" value="Enviar Datos"/> </p>
+		</form>
+	</body>
+
+</html>
