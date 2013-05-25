e55804f418d3323d21669ebeb3843de1f9874b3c
pedrocuevas <pcuevas24@gmail.com>
2013-05-25 01:38:47 -0400

    conexión con webservice

---
+++ 2.php
@@ -0,0 +1,28 @@
+﻿<!doctype html>
+
+<html lang = "es">
+
+	<head>
+	    <meta charset = "UTF-8">
+		<title>Autentificación</title>
+		<link rel="stylesheet" href="estilo.css" type="text/css" media="all">
+	</head>
+
+	
+	<body>
+       <?php
+         $rut = $_POST['usuario'];
+	     $password = $_POST['contraseña'];
+	
+	     $cliente = new SoapClient("http://informatica.utem.cl:8011/dirdoc-auth/ws/auth?wsdl");
+
+         $parametros = array("rut" => $rut , "password" => $password);
+
+         $consulta = $cliente->autenticar($parametros);
+	
+        ?>
+            <h1> <?php echo $consulta->return->descripcion; ?> </h1> 
+ 
+    </body>
+ 
+</html>
